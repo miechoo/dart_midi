1bc2b29e94e3d8d64a0e85fb5b8321351cf77554
Pawel Miechowiecki <my.subscriber.account@googlemail.com>
2020-12-23 01:39:04 +0100

    fixed path to the resource

--- test/midi_test.dart
+++ test/midi_test.dart
@@ -4,15 +4,17 @@ import 'package:test/test.dart';
 import 'package:dart_midi/dart_midi.dart';
 
 void main() {
-  test('parser and writer output matches', () {
-    var file = File('resources/MIDI_sample.mid');
-    final parser = MidiParser();
-    List<int> originalFileBuffer = file.readAsBytesSync();
-    var parsedMidi = parser.parseMidiFromBuffer(originalFileBuffer);
+  group('parser tests', () {
+    test('parser and writer output matches', () {
+      var file = File('./test/resources/MIDI_sample.mid');
+      final parser = MidiParser();
+      List<int> originalFileBuffer = file.readAsBytesSync();
+      var parsedMidi = parser.parseMidiFromBuffer(originalFileBuffer);
 
-    var writer = MidiWriter();
-    List<int> writtenBuffer = writer.writeMidiToBuffer(parsedMidi);
+      var writer = MidiWriter();
+      List<int> writtenBuffer = writer.writeMidiToBuffer(parsedMidi);
 
-    expect(originalFileBuffer, writtenBuffer);
+      expect(originalFileBuffer, writtenBuffer);
+    });
   });
 }
